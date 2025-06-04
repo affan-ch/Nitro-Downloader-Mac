78d9ff98d824091d05d98f29f000eb56dbc586cd
affan-ch <muhammadaffanmaqsood@gmail.com>
2025-06-04 23:27:52 +0500

    Initial Project

---
+++ Nitro Downloader/Nitro_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nitro_DownloaderApp.swift
+//  Nitro Downloader
+//
+//  Created by Affan Ali on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Nitro_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
